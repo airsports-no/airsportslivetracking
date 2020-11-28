71e3f1fd02ae73173b22519f7e57ad6cafba4f72
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2020-11-28 20:10:23 +0100

    updates token

--- src/secret_configuration.py
+++ src/secret_configuration.py
@@ -1,4 +1,4 @@
 # TRACCAR_ADDRESS = "home.kolaf.net:8082"
 TRACCAR_ADDRESS = "traccar:8082"
 PROTOCOL = "http"
-TOKEN = "FydcKTi7Lnat5wHhMGTCs0ykEpNAAOdj"
+TOKEN = "Fujvpqg8oJWrlhuaHcXXvfRIMfdwGThs"
