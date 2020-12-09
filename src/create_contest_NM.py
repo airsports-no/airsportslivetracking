bd52608f4413b47d0c37b9fb38343099f67389a7
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2020-12-09 13:48:21 +0100

    Fixed template

--- src/create_contest_NM.py
+++ src/create_contest_NM.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     import django
 
     django.setup()
-from display.models import Team, Aeroplane, NavigationTask, Track, Contestant, Scorecard, TraccarCredentials
+from display.models import Team, Aeroplane, NavigationTask, Track, Contestant, Scorecard, TraccarCredentials, Crew
 from display.default_scorecards.default_scorecard_fai_precision_2020 import get_default_scorecard
 
 configuration = TraccarCredentials.objects.get()
@@ -20,10 +20,9 @@ NavigationTask.objects.all().delete()
 aeroplane = Aeroplane.objects.first()
 contest_start_time = datetime.datetime(2020, 8, 1, 6, 0, 0).astimezone()
 contest_finish_time = datetime.datetime(2020, 8, 1, 16, 0, 0).astimezone()
-navigation_task = NavigationTask.objects.create(name="NM navigation_task",
-                                 track=Track.objects.get(name="NM 2020"),
-                                 start_time=contest_start_time, finish_time=contest_finish_time, wind_direction=165,
-                                 wind_speed=8)
+navigation_task = NavigationTask.objects.create(name="NM 2020 Navigation task",
+                                                track=Track.objects.get(name="NM 2020"),
+                                                start_time=contest_start_time, finish_time=contest_finish_time)
 contestants = {
     "Anders": (datetime.datetime(2020, 8, 1, 10, 0), 75, 1),
     "Arild": (datetime.datetime(2020, 8, 1, 10, 10), 70, 1),
@@ -56,7 +55,8 @@ scorecard = get_default_scorecard()
 
 for index, file in enumerate(glob.glob("../data/tracks/*.kml")):
     contestant_name = os.path.splitext(os.path.basename(file))[0]
-    team, _ = Team.objects.get_or_create(pilot=contestant_name, navigator="", aeroplane=aeroplane)
+    crew, _ = Crew.objects.get_or_create(pilot=contestant_name, navigator="")
+    team, _ = Team.objects.get_or_create(crew=crew, aeroplane=aeroplane)
     start_time, speed, contestant_class = contestants[contestant_name]
     print(contestant_name)
     print(start_time)
@@ -70,7 +70,8 @@ for index, file in enumerate(glob.glob("../data/tracks/*.kml")):
     contestant = Contestant.objects.create(navigation_task=navigation_task, team=team, takeoff_time=start_time,
                                            finished_by_time=start_time + datetime.timedelta(hours=2),
                                            traccar_device_name=contestant_name, contestant_number=index,
-                                           scorecard=scorecard, minutes_to_starting_point=6, air_speed=speed)
+                                           scorecard=scorecard, minutes_to_starting_point=6, air_speed=speed,
+                                           wind_direction=165, wind_speed=8)
 print(navigation_task.pk)
 # for contestant in Contestant.objects.filter(contest__pk = 7):
 #     contestant.takeoff_time = contestant.navigation_task.start_time
