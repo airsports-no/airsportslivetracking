a6cf324da78b39e6ef09632afa1192d3c5b4b5ab
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2020-12-25 23:09:20 +0100

    Replace pilots with team in contestant form

--- src/display/forms.py
+++ src/display/forms.py
@@ -276,35 +276,39 @@ class ClubSearchForm(forms.ModelForm):
 
 
 class ContestantForm(forms.ModelForm):
-    pilot_first_name = forms.CharField()
-    pilot_last_name = forms.CharField()
-    pilot_phone = PhoneNumberField(required=False)
-    pilot_email = forms.EmailField(required=False)
-
-    copilot_first_name = forms.CharField(required=False)
-    copilot_last_name = forms.CharField(required=False)
-    copilot_phone = PhoneNumberField(required=False)
-    copilot_email = forms.EmailField(required=False)
-
-    aircraft_registration = forms.CharField()
+    # pilot_first_name = forms.CharField()
+    # pilot_last_name = forms.CharField()
+    # pilot_phone = PhoneNumberField(required=False)
+    # pilot_email = forms.EmailField(required=False)
+    #
+    # copilot_first_name = forms.CharField(required=False)
+    # copilot_last_name = forms.CharField(required=False)
+    # copilot_phone = PhoneNumberField(required=False)
+    # copilot_email = forms.EmailField(required=False)
+    #
+    # aircraft_registration = forms.CharField()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance:
+            self.fields["team"].queryset = self.instance.navigation_task.contest.teams.all()
 
     class Meta:
         model = Contestant
         fields = (
-            "contestant_number", "tracker_start_time", "tracking_service", "traccar_device_name", "takeoff_time",
+            "contestant_number", "team", "tracker_start_time", "tracking_service", "traccar_device_name", "takeoff_time",
             "finished_by_time",
             "minutes_to_starting_point", "air_speed", "wind_direction", "wind_speed", "scorecard")
 
-    def save(self, commit=True):
-        contestant = super().save(commit=False)
-        member1 = Person.get_or_create(self.cleaned_data["pilot_first_name"], self.cleaned_data["pilot_last_name"],
-                                       self.cleaned_data["pilot_phone"], self.cleaned_data["pilot_email"])
-        member2 = Person.get_or_create(self.cleaned_data["copilot_first_name"], self.cleaned_data["copilot_last_name"],
-                                       self.cleaned_data["copilot_phone"], self.cleaned_data["copilot_email"])
-        crew, _ = Crew.objects.get_or_create(member1=member1, member2=member2)
-        aircraft, _ = Aeroplane.objects.get_or_create(registration=self.cleaned_data["aircraft_registration"])
-        team, _ = Team.objects.get_or_create(crew=crew, aeroplane=aircraft)
-        contestant.team = team
-        if commit:
-            contestant.save()
-        return contestant
+    # def save(self, commit=True):
+    #     contestant = super().save(commit=False)
+    #     member1 = Person.get_or_create(self.cleaned_data["pilot_first_name"], self.cleaned_data["pilot_last_name"],
+    #                                    self.cleaned_data["pilot_phone"], self.cleaned_data["pilot_email"])
+    #     member2 = Person.get_or_create(self.cleaned_data["copilot_first_name"], self.cleaned_data["copilot_last_name"],
+    #                                    self.cleaned_data["copilot_phone"], self.cleaned_data["copilot_email"])
+    #     crew, _ = Crew.objects.get_or_create(member1=member1, member2=member2)
+    #     aircraft, _ = Aeroplane.objects.get_or_create(registration=self.cleaned_data["aircraft_registration"])
+    #     team, _ = Team.objects.get_or_create(crew=crew, aeroplane=aircraft)
+    #     contestant.team = team
+    #     if commit:
+    #         contestant.save()
+    #     return contestant
