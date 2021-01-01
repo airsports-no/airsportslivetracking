401d34adc28549f9c818cebbc578782d22e04781
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2021-01-01 18:10:28 +0100

    Fixes name

--- src/display/default_scorecards/default_scorecard_fai_anr_2017.py
+++ src/display/default_scorecards/default_scorecard_fai_anr_2017.py
@@ -3,10 +3,10 @@ from display.models import GateScore, Scorecard
 
 
 def get_default_scorecard():
-    fai_anr_2020, created = Scorecard.objects.get_or_create(name="FAI ANR 2020")
+    scorecard, created = Scorecard.objects.get_or_create(name="FAI ANR 2017")
     if created:
-        fai_anr_2020.backtracking_penalty = 200
-        fai_anr_2020.backtracking_grace_time_seconds = 5
+        scorecard.backtracking_penalty = 200
+        scorecard.backtracking_grace_time_seconds = 5
         regular_gate_score = GateScore.objects.create(extended_gate_width=0.3,
                                                       bad_crossing_extended_gate_penalty=0,
                                                       graceperiod_before=1,
@@ -15,7 +15,7 @@ def get_default_scorecard():
                                                       penalty_per_second=3,
                                                       missed_penalty=300,
                                                       missed_procedure_turn_penalty=0)
-        fai_anr_2020.takeoff_gate_score = GateScore.objects.create(extended_gate_width=0,
+        scorecard.takeoff_gate_score = GateScore.objects.create(extended_gate_width=0,
                                                                    bad_crossing_extended_gate_penalty=0,
                                                                    graceperiod_before=0,
                                                                    graceperiod_after=60,
@@ -23,7 +23,7 @@ def get_default_scorecard():
                                                                    penalty_per_second=200,
                                                                    missed_penalty=0,
                                                                    missed_procedure_turn_penalty=0)
-        fai_anr_2020.landing_gate_score = GateScore.objects.create(extended_gate_width=0,
+        scorecard.landing_gate_score = GateScore.objects.create(extended_gate_width=0,
                                                                    bad_crossing_extended_gate_penalty=0,
                                                                    graceperiod_before=0,
                                                                    graceperiod_after=60,
@@ -31,9 +31,9 @@ def get_default_scorecard():
                                                                    penalty_per_second=0,
                                                                    missed_penalty=0,
                                                                    missed_procedure_turn_penalty=0)
-        fai_anr_2020.turning_point_gate_score = regular_gate_score
-        fai_anr_2020.secret_gate_score = regular_gate_score
-        fai_anr_2020.starting_point_gate_score = GateScore.objects.create(extended_gate_width=0.6,
+        scorecard.turning_point_gate_score = regular_gate_score
+        scorecard.secret_gate_score = regular_gate_score
+        scorecard.starting_point_gate_score = GateScore.objects.create(extended_gate_width=0.6,
                                                                           bad_crossing_extended_gate_penalty=200,
                                                                           graceperiod_before=2,
                                                                           graceperiod_after=2,
@@ -41,7 +41,7 @@ def get_default_scorecard():
                                                                           penalty_per_second=3,
                                                                           missed_penalty=100,
                                                                           missed_procedure_turn_penalty=200)
-        fai_anr_2020.finish_point_gate_score = regular_gate_score
-        fai_anr_2020.save()
+        scorecard.finish_point_gate_score = regular_gate_score
+        scorecard.save()
 
-    return fai_anr_2020
+    return scorecard
