d4970bfd3d78370006ca503ec797644dbbbe31fb
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2021-01-05 17:11:46 +0100

    fixes

--- src/live_tracking_map/asgi.py
+++ src/live_tracking_map/asgi.py
@@ -13,9 +13,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import display.routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'live_tracking_map.settings')
+
+import display.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
