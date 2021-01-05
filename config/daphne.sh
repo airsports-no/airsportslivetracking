7e81a6e858bbe5a8b29910666a1f1889d886ed27
Frank Olaf Sem-Jacobsen <frank@statsat.no>
2021-01-05 17:00:35 +0100

    fixes

--- config/daphne.sh
+++ config/daphne.sh
@@ -5,7 +5,7 @@ DJANGODIR=/src/                               # Django project directory (*)
 NUM_WORKERS=5                                # how many worker processes should Gunicorn spawn (*)
 NUM_THREADS=2                                 # How many threads should each worker have
 DJANGO_SETTINGS_MODULE=live_tracking_map.settings  # which settings file should Django use (*)
-DJANGO_WSGI_MODULE=live_tracking_map.wsgi          # WSGI module name (*)
+DJANGO_ASGI_MODULE=live_tracking_map.asgi          # ASGI module name (*)
 
 echo "Starting $NAME as `whoami`"
 
@@ -20,4 +20,4 @@ export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec daphne -b 0.0.0.0 -p 8003 ${DJANGO_WSGI_MODULE}.asgi:application
+exec daphne -b 0.0.0.0 -p 8003 ${DJANGO_WSGI_MODULE}:application
