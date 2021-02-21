Fixes corridor polygon calculation for rounded corners (and pointy corners, I think)

src/display/calculators/anr_corridor_calculator.py
@@ -56,12 +56,16 @@ class AnrCorridorCalculator(Calculator):
 
     def build_polygon(self):
         points = []
-
         for waypoint in self.contestant.navigation_task.route.waypoints:
-            # latitude, longitude, so reverse
-            points.append(list(reversed(waypoint.gate_line[0])))
+            if self.contestant.navigation_task.route.rounded_corners:
+                points.append(waypoint.left_corridor_line)
+            else:
+                points.append(waypoint.gate_line[0])
         for waypoint in reversed(self.contestant.navigation_task.route.waypoints):
-            points.append(list(reversed(waypoint.gate_line[1])))
+            if self.contestant.navigation_task.route.rounded_corners:
+                points.append(waypoint.right_corridor_line)
+            else:
+                points.append(waypoint.gate_line[1])
         points = np.array(points)
         print(points.shape)
         transformed_points = self.epsg.transform_points(self.pc, points[:, 0], points[:, 1])

src/display/coordinate_utilities.py
@@ -331,7 +331,7 @@ def create_rounded_corridor_corner(bisecting_line: Tuple[Tuple[float, float], Tu
     :param corridor_width: The width of the corridor NM
     :paramcorner_degrees: The number of degrees for the turn
     :param bisecting_line: The line that bisects the middle of the turn
-    :return: List of points that make up the left-hand corridor line, list of points that make up the right-hand corridor line
+    :return: List of points that make up the left-hand corridor line, list of points that make up the right-hand corridor line (lon, lat)
     """
     if corner_degrees == 0:
         return [bisecting_line[0]], [bisecting_line[1]]
@@ -454,7 +454,7 @@ def create_bisecting_line_between_segments_corridor_width_xy(x1, y1, x2, y2, x3,
     :param x3:
     :param y3:
     :param length: metres
-    :return:
+    :return: lon, lat
     """
     l1 = np.array([x1, y1])
     l2 = np.array([x2, y2])