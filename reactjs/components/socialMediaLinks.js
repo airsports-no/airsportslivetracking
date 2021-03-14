bb07be74ed5b62ac1cfdefb5ff3cf2d0dea55777
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-03-14 19:37:20 +0100

    Social media to new tab

--- reactjs/components/socialMediaLinks.js
+++ reactjs/components/socialMediaLinks.js
@@ -5,16 +5,16 @@ import Icon from "@mdi/react";
 export class SocialMediaLinks extends Component {
     render() {
         return <div className={"social-media-box"}>
-            <a href={"https://www.instagram.com/AirSportsLive"}><Icon path={mdiInstagram} title={"Instagram"} size={1.1}
+            <a href={"https://www.instagram.com/AirSportsLive"} target={"_blank"}><Icon path={mdiInstagram} title={"Instagram"} size={1.1}
                                                                       color={"black"}/></a>
-            <a href={"https://www.twitter.com/AirSportsLive"}><Icon path={mdiTwitter} title={"Twitter"} size={1.1}
+            <a href={"https://www.twitter.com/AirSportsLive"} target={"_blank"}><Icon path={mdiTwitter} title={"Twitter"} size={1.1}
                                                                     color={"black"}/></a>
-            <a href={"https://www.facebook.com/AirSportsLive"}><Icon path={mdiFacebook} title={"Facebook"} size={1.1}
+            <a href={"https://www.facebook.com/AirSportsLive"} target={"_blank"}><Icon path={mdiFacebook} title={"Facebook"} size={1.1}
                                                                      color={"black"}/></a>
             <a href={"https://www.youtube.com/channel/UCgKCfAzU9wl42wnb1Tj_SCA"}><Icon path={mdiYoutube}
                                                                                        title={"Youtube"} size={1.1}
                                                                                        color={"black"}/></a>
-            <a href={"mailto:support@airsports.no"}><Icon path={mdiMail} title={"E-mail"} size={1.1}
+            <a href={"mailto:support@airsports.no"} target={"_blank"}><Icon path={mdiMail} title={"E-mail"} size={1.1}
                                                           color={"black"}/></a>
 
         </div>
