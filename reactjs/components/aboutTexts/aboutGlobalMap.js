4bc5fdb925092e58beaa0717f884e88e23b5287f
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-04-11 20:12:43 +0200

    About tweaks

--- reactjs/components/aboutTexts/aboutGlobalMap.js
+++ reactjs/components/aboutTexts/aboutGlobalMap.js
@@ -10,23 +10,22 @@ const aboutGlobalMap = <div>
     <img src={"/static/img/airsports_no_text.png"} style={{float: "right", width: "40px"}} alt={"Global logo"}/>
     <h4>Global map - tracking and events</h4>
     <ul>
-        <li>Use Live Tracking to sharing your position!</li>
-        <li>Help yourself and other and be visible on the global map.</li>
+        <li>Use Live Tracking to share your position!</li>
+        <li>Help yourself and others and be visible on the global map.</li>
         <li>Get an overview of ongoing and upcoming events.</li>
         <li>Clicking on the event, and jump to the event details.</li>
     </ul>
     <p>
-        Map Aircraft Symbolization
+        Map aircraft symbols
         <table className={"table borderless"}>
             <tbody>
             <tr>
-                <td><i className="mdi mdi-airplanemode-active" style={{color: "blue"}}/> Active aircraft</td>
-                <td>Speed: GPS speed in KTS.</td>
+                <td style={{width: "160px"}}><i className="mdi mdi-airplanemode-active" style={{color: "blue"}}/> Active aircraft</td>
+                <td rowSpan={3}>Speed: GPS in KTS.<br/>Altitude: GPS nearest 100 feet</td>
             </tr>
             <tr>
                 <td><i className="mdi mdi-airplanemode-active" style={{color: "blue", opacity: 0.4}}/> &lt; 40 knots
                 </td>
-                <td>Altitude: GPS altitude nearest 100 feet</td>
             </tr>
             <tr>
                 <td><i className="mdi mdi-airplanemode-active" style={{color: "grey", opacity: 0.4}}/> &gt; 20 sec old
@@ -43,13 +42,13 @@ const aboutGlobalMap = <div>
         <Icon path={mdiGoKartTrack} title={"Tracking"} size={1.5} color={"#e01b1c"}
               style={{float: "right", width: "50px"}}/>
         <h4>Competition Flying - with real-time scoring</h4>
-        With airsports.no you can create competitions like Precision Flying, Rally Flying or Air Navigation Racing.
-        Organizers can use this feature for free, but it requires access. Contact us for more information at
-        support[at]airsports.no. The contestants only need to use the app for tracking in competition mode.
+        With airsports.no you can create competitions like Precision Flying, Rally Flying, or Air Navigation Racing.
+        Organizers can use this feature for free, so contact us at
+        support[at]airsports.no to become an organizer. The contestants only need the app to compete in the contest, no hardware tracker required.
     </p>
     <p>
-        <a href={"https://youtu.be/4ZPlDVjXabs"}>Here you can watch a tutorial on how to make a competition!</a><br/>
-        Please visit and subscribe to our YouTube Channel for more videos.
+        Take a look at our <a href={"https://youtu.be/4ZPlDVjXabs"}>competition creation tutorial</a>, and
+        please visit and subscribe to our YouTube Channel for more videos.
     </p>
     <hr/>
     <p>
