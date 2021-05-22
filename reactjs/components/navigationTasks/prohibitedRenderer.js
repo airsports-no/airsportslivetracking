ed1ceaeb71d055b54bc93f3b7bb7466a8cd57e93
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-05-22 16:58:08 +0200

    prohibited tooltip styling

--- reactjs/components/navigationTasks/prohibitedRenderer.js
+++ reactjs/components/navigationTasks/prohibitedRenderer.js
@@ -11,13 +11,13 @@ export default class ProhibitedRenderer extends Component {
         this.props.navigationTask.route.prohibited_set.map((polygonDefinition) => {
             if (polygonDefinition.type === "prohibited") {
                 let p = polygon(polygonDefinition.path, {color: "red"}).addTo(this.props.map)
-                p.bindTooltip(polygonDefinition.name, {permanent: true, direction: "center"}).openTooltip()
+                p.bindTooltip(polygonDefinition.name, {permanent: true, direction: "center", className: "prohibitedTooltip"}).openTooltip()
             } else if (polygonDefinition.type === "info") {
                 let p = polygon(polygonDefinition.path, {color: "orange"}).addTo(this.props.map)
-                p.bindTooltip(polygonDefinition.name, {permanent: true, direction: "center"}).openTooltip()
+                p.bindTooltip(polygonDefinition.name, {permanent: true, direction: "center", className: "prohibitedTooltip"}).openTooltip()
             } else if (polygonDefinition.type === "gate") {
                 let p = polygon(polygonDefinition.path, {color: "blue"}).addTo(this.props.map)
-                p.bindTooltip(polygonDefinition.name, {permanent: false, direction: "center"})
+                p.bindTooltip(polygonDefinition.name, {permanent: false, direction: "center", className: "prohibitedTooltip"})
             }
         })
     }
