cb1dda6aa2babdda90341ef93750062cb0e1e5a5
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-06-12 12:45:28 +0200

    Debug information

--- src/display/contestant_scheduler.py
+++ src/display/contestant_scheduler.py
@@ -88,9 +88,8 @@ class Solver:
         return self.teams
 
     def dump_solution(self):
-        teams = sorted(self.teams, key=lambda t: self.team_map[t.pk].start_slot)
-        for team_definition in teams:
-            team = self.team_map[team_definition.pk]
+        teams = sorted(self.teams, key=lambda t: t.start_slot if t.start_slot else -1)
+        for team in teams:
             logger.info(f"Team {team} will start in slot {team.start_slot} at {team.start_time}")
 
 
