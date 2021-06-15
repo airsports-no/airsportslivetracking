8b0b55ebb46657817701955e86bb136edafd134e
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-06-15 11:22:57 +0200

    Prohibited penalty changed to 200 for ANR

--- src/display/contestant_scheduler.py
+++ src/display/contestant_scheduler.py
@@ -84,7 +84,7 @@ class Solver:
             self.team_map[team.pk].start_time = self.first_takeoff_time + datetime.timedelta(
                 minutes=slot * self.minutes_per_slot)
             self.team_map[team.pk].start_slot = slot
-            self.dump_solution()
+        self.dump_solution()
         return self.teams
 
     def dump_solution(self):
