10b78ea6c7bbaf5a1533fe96ab9a8bd98763bad4
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-06-20 14:40:58 +0200

    Reform of code

--- src/display/models.py
+++ src/display/models.py
@@ -349,6 +349,7 @@ class Team(models.Model):
     @property
     def table_display(self):
         return f"{self.crew.table_display}<br/>{self.aeroplane}"
+
     @property
     def country_flag_url(self):
         if self.country:
@@ -1762,9 +1763,15 @@ class Contestant(models.Model):
         devices = []
         if self.tracking_device == TRACKING_DEVICE:
             devices.append(self.tracker_device_id)
-        if self.tracking_device in (TRACKING_PILOT, TRACKING_PILOT_AND_COPILOT) and self.team.crew.member1 is not None:
+        if (
+            self.tracking_device in (TRACKING_PILOT, TRACKING_PILOT_AND_COPILOT)
+            and self.team.crew.member1 is not None
+        ):
             devices.append(self.team.crew.member1.email)
-        if self.tracking_device in (TRACKING_COPILOT, TRACKING_PILOT_AND_COPILOT) and self.team.crew.member2 is not None:
+        if (
+            self.tracking_device in (TRACKING_COPILOT, TRACKING_PILOT_AND_COPILOT)
+            and self.team.crew.member2 is not None
+        ):
             devices.append(self.team.crew.member2.email)
         logger.error(
             f"Contestant {self.team} for navigation task {self.navigation_task} does not have a tracker ID for tracking device {self.tracking_device}"
