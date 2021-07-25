e16075903f0f0f7efd82b73822455876ab39d2b8
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2021-07-25 17:17:49 +0200

    Cleans up formatting

--- reactjs/components/timeDisplay.js
+++ reactjs/components/timeDisplay.js
@@ -4,16 +4,14 @@ import {connect} from "react-redux";
 const {formatTime} = require("../utilities");
 const mapStateToProps = (state, props) => ({
     latestPositionTime: props.contestantId ? state.contestantData[props.contestantId].latest_position_time : null,
-    finished: props.contestantId ? state.contestantData[props.contestantId].calculator_finished || state.contestantData[props.contestantId].current_state === "Finished" : false,
+    finished: props.contestantId && (state.contestantData[props.contestantId].calculator_finished || state.contestantData[props.contestantId].current_state === "Finished"),
     currentTime: state.currentTime
 })
 
 class ConnectedTimeDisplay extends Component {
     render() {
-        if (this.props.latestPositionTime && !this.props.finished) {
-            return <div className={this.props.class}>{formatTime(this.props.latestPositionTime)}</div>
-        }
-        return <div className={this.props.class}>{this.props.currentTime}</div>
+        return <div
+            className={this.props.class}>{this.props.latestPositionTime && !this.props.finished ? formatTime(this.props.latestPositionTime) : this.props.currentTime}</div>
     }
 }
 
