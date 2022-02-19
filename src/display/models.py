319fe6a3cca065985d6acd7ab026c7238140b175
Frank Olaf Sem-Jacobsen <frank.sem-jacobsen@statsat.no>
2022-02-19 11:25:53 +0100

    Capitalises gate names #654

--- src/display/models.py
+++ src/display/models.py
@@ -96,14 +96,14 @@ LANDING_GATE = "ldg"
 INTERMEDIARY_STARTINGPOINT = "isp"
 INTERMEDIARY_FINISHPOINT = "ifp"
 GATE_TYPES = (
-    (TURNPOINT, "Turning point"),
-    (STARTINGPOINT, "Starting point"),
-    (FINISHPOINT, "Finish point"),
-    (SECRETPOINT, "Secret point"),
-    (TAKEOFF_GATE, "Takeoff gate"),
-    (LANDING_GATE, "Landing gate"),
-    (INTERMEDIARY_STARTINGPOINT, "Intermediary starting point"),
-    (INTERMEDIARY_FINISHPOINT, "Intermediary finish point"),
+    (TURNPOINT, "Turning Point"),
+    (STARTINGPOINT, "Starting Point"),
+    (FINISHPOINT, "Finish Point"),
+    (SECRETPOINT, "Secret Point"),
+    (TAKEOFF_GATE, "Takeoff Gate"),
+    (LANDING_GATE, "Landing Gate"),
+    (INTERMEDIARY_STARTINGPOINT, "Intermediary Starting Point"),
+    (INTERMEDIARY_FINISHPOINT, "Intermediary Finish Point"),
 )
 
 TRACKING_DEVICE_TIMEOUT = 10
