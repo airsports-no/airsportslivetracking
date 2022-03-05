9587bf06f8f73aa3d5afdcefd5af888e145c5339
Frank Olaf Sem-Jacobsen <frank.sem-jacobsen@statsat.no>
2022-03-05 21:22:53 +0100

    Fixes embedded video #658

--- reactjs/components/aboutTexts/aboutGlobalMap.js
+++ reactjs/components/aboutTexts/aboutGlobalMap.js
@@ -58,7 +58,7 @@ const aboutGlobalMap = <div>
         <img src={"/static/img/airsports_help.png"} style={{float: "right", width: "40px", marginTop: "-10px"}} alt={"Global logo"}/>
         <b>Air Sports Race introduction</b>
         <div className="video-container">
-            <iframe src="https://www.youtube.com/watch?v=jywBZyqcL1Q"
+            <iframe src="https://www.youtube.com/embed/jywBZyqcL1Q"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     frameBorder="0" allowFullScreen className="video"/>
         </div>
