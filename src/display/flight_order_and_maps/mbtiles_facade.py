7932915f6eeabc9f3e1e1e9bb1e5c2e1d2021644
Frank Olaf Sem-Jacobsen <frank.sem-jacobsen@statsat.no>
2023-06-14 19:22:18 +0200

    Adds retries and changes to spot instance

--- src/display/flight_order_and_maps/mbtiles_facade.py
+++ src/display/flight_order_and_maps/mbtiles_facade.py
@@ -4,10 +4,33 @@ from live_tracking_map.settings import MBTILES_SERVER_URL
 
 
 def get_available_maps() -> dict:
-    result = requests.get(f"{MBTILES_SERVER_URL}services/")
+    result = _requests_retry_session().get(f"{MBTILES_SERVER_URL}services/")
     return result.json()
 
 
 def get_map_details(map_key: str) -> dict:
-    result = requests.get(f"{MBTILES_SERVER_URL}services/{map_key}")
+    result = _requests_retry_session().get(f"{MBTILES_SERVER_URL}services/{map_key}")
     return result.json()
+
+
+def _requests_retry_session(
+    retries=10,
+    backoff_factor=0.5,
+    status_forcelist=(502,),
+    session=None,
+):
+    """
+    Request with retry. Borrowed from https://www.peterbe.com/plog/best-practice-with-retries-with-requests
+    """
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
