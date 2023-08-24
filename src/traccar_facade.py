5c9e0be3456b4f3bbb9cc49c9d5239815b1ee518
FrankOlafSem-Jacobsen <frankose@ifi.uio.no>
2023-08-24 20:57:53 +0200

    Imposes one-hour session lifetime to avoid timeout

--- src/traccar_facade.py
+++ src/traccar_facade.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import time
 from typing import List, Dict, TYPE_CHECKING, Optional, Tuple
 
 import requests
@@ -18,6 +19,8 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
+SESSION_LIFETIME = 3600
+
 
 class Traccar:
     def __init__(self, protocol, address, username, password):
@@ -26,7 +29,8 @@ class Traccar:
         self.username = username
         self.password = password
         self.base = "{}://{}".format(self.protocol, self.address)
-        self.session = self.get_authenticated_session()
+        self._session = self.get_authenticated_session()
+        self.last_session_time = None
         self.device_map = {}
         self.unique_id_map = {}
 
@@ -39,6 +43,16 @@ class Traccar:
             TRACCAR_PASSWORD,
         )
 
+    @property
+    def session(self) -> Session:
+        if not self._session or time.time() - SESSION_LIFETIME > self.last_session_time:
+            try:
+                self._session.close()
+            except:
+                pass
+            self._session = self.get_authenticated_session()
+        return self._session
+
     def get_authenticated_session(self) -> Session:
         session = requests.Session()
         response = session.post(
@@ -48,6 +62,7 @@ class Traccar:
         # response = session.get(string)
         if response.status_code != 200:
             raise Exception("Failed authenticating session: {}".format(response.text))
+        self.last_session_time = time.time()
         return session
 
     def get_positions_for_device_id(
@@ -96,16 +111,12 @@ class Traccar:
         if response.status_code == 200:
             return response.json()
         else:
-            logger.error(
-                f"Failed fetching positions for device {device_id}, {response.text}"
-            )
+            logger.error(f"Failed fetching positions for device {device_id}, {response.text}")
             return []
 
     def get_device_ids_for_contestant(self, contestant: "Contestant") -> List[int]:
         devices = []
-        for name in (
-            contestant.get_tracker_ids() + contestant.get_simulator_tracker_ids()
-        ):
+        for name in contestant.get_tracker_ids() + contestant.get_simulator_tracker_ids():
             try:
                 devices.append(self.unique_id_map[name])
             except KeyError:
@@ -136,9 +147,7 @@ class Traccar:
             return response.json()
 
     def create_group(self, group_name) -> Dict:
-        response = self.session.post(
-            self.base + "/api/groups", json={"name": group_name}
-        )
+        response = self.session.post(self.base + "/api/groups", json={"name": group_name})
         if response.status_code == 200:
             return response.json()
 
@@ -172,9 +181,7 @@ class Traccar:
             return True
 
     def get_device(self, identifier) -> Optional[Dict]:
-        response = self.session.get(
-            self.base + "/api/devices/?uniqueId={}".format(identifier)
-        )
+        response = self.session.get(self.base + "/api/devices/?uniqueId={}".format(identifier))
         if response.status_code == 200:
             devices = response.json()
             try:
@@ -187,9 +194,7 @@ class Traccar:
         existing_device = self.get_device(identifier)
         logger.debug(f" Found existing device {existing_device}")
         if existing_device is None:
-            logger.warning(
-                "Failed fetching assumed to be existing device {}".format(identifier)
-            )
+            logger.warning("Failed fetching assumed to be existing device {}".format(identifier))
             return False
         key = existing_device["id"]
         response = self.session.put(
@@ -197,9 +202,7 @@ class Traccar:
             json={"name": device_name, "id": key, "uniqueId": identifier},
         )
         if response.status_code != 200:
-            logger.error(
-                f"Failed updating device name because of: {response.status_code} {response.text}"
-            )
+            logger.error(f"Failed updating device name because of: {response.status_code} {response.text}")
             return False
         logger.debug(f"Updated device name for {identifier} to {device_name}")
         return True
@@ -218,8 +221,6 @@ class Traccar:
 
     def get_device_map(self) -> Dict:
         if dmap := self.update_and_get_devices():
-            self.device_map = {
-                item["id"]: item["uniqueId"] for item in dmap
-            }
+            self.device_map = {item["id"]: item["uniqueId"] for item in dmap}
         self.unique_id_map = {value: key for key, value in self.device_map.items()}
         return self.device_map
