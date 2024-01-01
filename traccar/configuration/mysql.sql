35ed315d639b312bf9c38df5b28408409aeb222c
FrankOlafSem-Jacobsen <frankose@ifi.uio.no>
2024-01-01 18:34:51 +0100

    Removes unused traccar config

--- traccar/configuration/mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS traccar;
-CREATE DATABASE IF NOT EXISTS tracker;
-
-CREATE USER 'traccar'@'%' IDENTIFIED BY 'traccar';
-CREATE USER 'tracker'@'%' IDENTIFIED BY 'tracker';
-
-GRANT ALL PRIVILEGES ON traccar.* To 'traccar'@'%';
-GRANT ALL PRIVILEGES ON *.* To 'tracker'@'%';
-FLUSH PRIVILEGES;
