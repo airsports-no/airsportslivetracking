7c03b56ecfb3f1ceac8a84f7c21c906ae01b07f1
FrankOlafSem-Jacobsen <frankose@ifi.uio.no>
2024-01-01 20:27:02 +0100

    Revert "Removes unused traccar config"
    This reverts commit 35ed315d639b312bf9c38df5b28408409aeb222c.

---
+++ traccar/configuration/mysql.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS traccar;
+CREATE DATABASE IF NOT EXISTS tracker;
+
+CREATE USER 'traccar'@'%' IDENTIFIED BY 'traccar';
+CREATE USER 'tracker'@'%' IDENTIFIED BY 'tracker';
+
+GRANT ALL PRIVILEGES ON traccar.* To 'traccar'@'%';
+GRANT ALL PRIVILEGES ON *.* To 'tracker'@'%';
+FLUSH PRIVILEGES;
