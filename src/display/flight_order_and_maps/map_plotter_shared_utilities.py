188977e260806d2ba499ce56079f17b07a849e5a
FrankOlafSem-Jacobsen <frankose@ifi.uio.no>
2024-02-21 17:49:12 +0100

    Use map filename format name from mbtiles server.

--- src/display/flight_order_and_maps/map_plotter_shared_utilities.py
+++ src/display/flight_order_and_maps/map_plotter_shared_utilities.py
@@ -3,6 +3,11 @@ import qrcode
 
 from display.flight_order_and_maps.mbtiles_facade import get_available_maps, get_map_details
 
+SWEDEN_250 = "Sweden250k"
+SWEDEN_100 = "Sweden100k"
+NORWAY_250 = "Norway250k"
+FINLAND_200 = "Finland200k"
+
 
 def folder_map_name(folder: str) -> str:
     actual_map = folder.split("/")[-1]
@@ -10,8 +15,12 @@ def folder_map_name(folder: str) -> str:
     return " ".join([item.capitalize() for item in elements])
 
 
+def get_map_filename(url: str) -> str:
+    return url.split("/")[-1]
+
+
 def get_map_choices() -> list[tuple[str, str]]:
-    return [(item["url"].split("/")[-1], item["name"]) for item in get_available_maps()] + [
+    return [(get_map_filename(item["url"]), get_map_filename(item["url"])) for item in get_available_maps()] + [
         ("osm", "OSM"),
         ("fc", "Flight Contest"),
         ("mto", "MapTiler Outdoor"),
@@ -20,15 +29,15 @@ def get_map_choices() -> list[tuple[str, str]]:
 
 
 def country_code_to_map_source(country_code: str) -> str:
-    return {"no": "Norway250k", "fi": "Finland200k", "se": "Sweden100k"}.get(country_code, "cyclosm")
+    return {"no": NORWAY_250, "fi": FINLAND_200, "se": SWEDEN_250}.get(country_code, "cyclosm")
 
 
-DEFAULT_MAP_ZOOM_LEVELS = {"Norway250k": 12, "Finland200k": 12, "Sweden100k": 12}
+DEFAULT_MAP_ZOOM_LEVELS = {NORWAY_250: 12, FINLAND_200: 12, SWEDEN_100: 12, SWEDEN_250: 12}
 MAP_ATTRIBUTIONS = {
-    "Norway250k": "Contains data from kartverket.no, 07/2023",
-    "Finland200k": "Contains data from the National Land Survey of Finland Topographic Database 07/2023",
-    "Sweden100k": "Contains data from lentmateriet.se, 07/2023",
-    "Sverige": "Contains data from lentmateriet.se, 02/2024",
+    NORWAY_250: "Contains data from kartverket.no, 07/2023",
+    FINLAND_200: "Contains data from the National Land Survey of Finland Topographic Database 07/2023",
+    SWEDEN_100: "Contains data from lentmateriet.se, 07/2023",
+    SWEDEN_250: "Contains data from lentmateriet.se, 02/2024",
 }
 
 
