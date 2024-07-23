471d3fd6e918e926a1fad31e8d7504d314319280
FrankOlafSem-Jacobsen <frankose@ifi.uio.no>
2024-07-23 11:01:41 +0200

    Updates ContestantReceivedPosition

--- src/display/models/contestant_utility_models.py
+++ src/display/models/contestant_utility_models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from display.calculators.positions_and_gates import Position
 from display.fields.my_pickled_object_field import MyPickledObjectField
 
 
@@ -9,6 +8,7 @@ class ContestantUploadedTrack(models.Model):
     Store a gps track that has been manually uploaded through the API or GUI. They can only be one of these for a
     contestant, and if this is present it will be preferred over anything received through traccar.
     """
+
     contestant = models.OneToOneField("Contestant", on_delete=models.CASCADE)
     track = MyPickledObjectField(default=list, help_text="List of traccar position reports (Dict)")
 
@@ -18,13 +18,21 @@ class ContestantReceivedPosition(models.Model):
     Represents a position received from traccar. Includes timestamps that can be used to calculate processing
     statistics.
     """
+
     contestant = models.ForeignKey("Contestant", on_delete=models.CASCADE)
+
     time = models.DateTimeField()
     latitude = models.FloatField()
     longitude = models.FloatField()
     altitude = models.FloatField(default=0)
-    course = models.FloatField()
     speed = models.FloatField(default=0)
+    course = models.FloatField(default=0)
+
+    battery_level = models.FloatField(default=0)
+    position_id = models.IntegerField(default=0)
+    device_id = models.TextField(default="unknown")
+    progress = models.FloatField(default=0)
+
     interpolated = models.BooleanField(default=False)
     processor_received_time = models.DateTimeField(blank=True, null=True)
     calculator_received_time = models.DateTimeField(blank=True, null=True)
@@ -34,32 +42,15 @@ class ContestantReceivedPosition(models.Model):
     class Meta:
         ordering = ("time",)
 
-    @staticmethod
-    def convert_to_traccar(positions: list["ContestantReceivedPosition"]) -> list[Position]:
-        """
-        Converts the list of contestant received positions to a list of traccar positions dictionaries
-        """
-        try:
-            contestant = positions[0].contestant
-        except IndexError:
-            return []
-        return [
-            Position(
-                **contestant.generate_position_block_for_contestant(
-                    {
-                        "deviceId": contestant.tracker_device_id,
-                        "id": index,
-                        "latitude": float(point.latitude),
-                        "longitude": float(point.longitude),
-                        "altitude": 0,
-                        "attributes": {"batteryLevel": 1.0},
-                        "speed": 0.0,
-                        "course": point.course,
-                        "device_time": point.time,
-                    },
-                    point.time,
-                ),
-                interpolated=point.interpolated,
-            )
-            for index, point in enumerate(positions)
-        ]
+    def to_traccar(self, device_id: str, index: int) -> dict:
+        return {
+            "deviceId": device_id,
+            "id": index,
+            "latitude": float(self.latitude),
+            "longitude": float(self.longitude),
+            "altitude": self.latitude,
+            "attributes": {"batteryLevel": self.battery_level},
+            "speed": self.speed,
+            "course": self.course,
+            "device_time": self.time,
+        }
