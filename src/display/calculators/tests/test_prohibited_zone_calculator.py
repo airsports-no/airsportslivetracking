a1c61a145530aba3fbabc7b50484456d3bd571a4
Frank Olaf Sem-Jacobsen <frankose@ifi.uio.no>
2025-04-20 15:57:50 +0000

    Fixes test

--- src/display/calculators/tests/test_prohibited_zone_calculator.py
+++ src/display/calculators/tests/test_prohibited_zone_calculator.py
@@ -60,7 +60,7 @@ class TestProhibitedZoneCalculator(TransactionTestCase):
                 11.5,
                 "anomaly",
                 "inside_prohibited_zone",
-                maximum_score=-1,
+                maximum_score=0,
             )
         )
 
