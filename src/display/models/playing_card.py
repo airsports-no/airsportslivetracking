2ce4e96accefd5796b6e3973091bd92ac14590e9
Frank Olaf Sem-Jacobsen <frank@airsports.no>
2025-04-25 07:20:29 +0000

    Show relative score changes in score log

--- src/display/models/playing_card.py
+++ src/display/models/playing_card.py
@@ -90,7 +90,7 @@ class PlayingCard(models.Model):
             time=datetime.datetime.now(datetime.timezone.utc),
             gate=waypoint,
             message=message,
-            points=new_hand_score,
+            points=previous_hand_score + new_hand_score,
             string="{}: {}".format(waypoint, message),
         )
 
@@ -123,7 +123,7 @@ class PlayingCard(models.Model):
                 time=datetime.datetime.now(datetime.timezone.utc),
                 gate=waypoint,
                 message=message,
-                points=new_hand_score,
+                points=previous_hand_score + new_hand_score,
                 string="{}: {}".format(waypoint, message),
             )
 
@@ -158,7 +158,7 @@ class PlayingCard(models.Model):
             time=datetime.datetime.now(datetime.timezone.utc),
             gate=waypoint,
             message=message,
-            points=new_hand_score,
+            points=previous_hand_score + new_hand_score,
             type=ANOMALY,
             string="{}: {}".format(waypoint, message),
         )
