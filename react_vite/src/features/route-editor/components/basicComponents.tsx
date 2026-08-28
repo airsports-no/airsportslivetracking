02aba1dede6dfef321da575c1381cd7dc56a4ef9
Frank Olaf Sem-Jacobsen <frank@airsports.no>
2026-08-28 14:17:39 +0200

    Review base: remove frontend features for scoped ultra review

--- react_vite/src/features/route-editor/components/basicComponents.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const Loading: React.FC = () => <div className="flex justify-center"><img className="loading-lg" src={document.configuration.STATIC_FILE_LOCATION+"img/loading_airplane.gif"} alt="loading..."/></div>;
